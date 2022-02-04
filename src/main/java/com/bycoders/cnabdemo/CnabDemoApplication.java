9fa6e2b02fa5dc30f48e0c87815c3fa282a65a93
Patrik <patrik.pergher@pratidonaduzzi.com.br>
2022-02-04 09:38:02 -0300

    First commit
    Carregando o projeto

---
+++ src/main/java/com/bycoders/cnabdemo/CnabDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bycoders.cnabdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnabDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnabDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bycoders/cnabdemo/CnabDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bycoders.cnabdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnabDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
