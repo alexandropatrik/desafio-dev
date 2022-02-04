531ddbb75e6cdb7e2205f187b462743c481f408d
alexandropatrik <alexandropatrik@gmail.com>
2022-02-04 16:11:12 -0300

    Teste para leitura e persistência dos dados

--- src/test/java/com/bycoders/cnabdemo/CnabDemoApplicationTests.java
+++ src/test/java/com/bycoders/cnabdemo/CnabDemoApplicationTests.java
@@ -1,13 +1,23 @@
 package com.bycoders.cnabdemo;
 
+import com.bycoders.cnabdemo.services.TransacaoFinanceiraService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CnabDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private TransacaoFinanceiraService transacaoFinanceiraService;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void processar() {
+        transacaoFinanceiraService.processar();
+    }
 
 }
